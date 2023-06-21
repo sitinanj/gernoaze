d054081b04f9dac9ba72b60440c8e23dc997a0c1
sjacobus <sitinan.jacobus@gmail.com>
2023-06-21 10:13:59 +0200

    fix maybe

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import cloudflare from "@astrojs/cloudflare";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  /*output: 'hybrid',
-  adapter: cloudflare()*/
+  output: 'hybrid',
+  adapter: cloudflare()
 });
