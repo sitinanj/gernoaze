17b47086ebe0fde17b02cd533093ba9d67f83dc4
sjacobus <sitinan.jacobus@gmaiL.com>
2023-06-21 12:57:53 +0200

    stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
+
+// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
